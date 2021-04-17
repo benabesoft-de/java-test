99241bb7dd15e03aa6a560d2dad9eb8552f34ca1
BenjaminAbe <benjamin@benabesoft.de>
2021-04-17 18:23:27 +0200

    Ordner hinzugefügt

---
+++ code/java-test/src/java/test/JavaTest.java
@@ -0,0 +1,22 @@
+// * * * * * * * * * * * * * * * * * * * * * * * * 
+// * Software von:                               *
+// *                 benabesoft.de               *
+// * * * * * * * * * * * * * * * * * * * * * * * * 
+// Alle Rechte vorbehalten
+// * * * * * * * * * * * * * * * * * * * * * * * * 
+package java.test;
+
+/**
+ *
+ * @author benjamin
+ */
+public class JavaTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
